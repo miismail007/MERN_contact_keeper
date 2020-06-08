fd87418be16a09446b23aa88d96f0609d25fa503
Mohammed Ismail <miismail007@gmail.com>
2020-06-08 18:40:56 +0530

    add json web token for registration

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -38,8 +40,24 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-
-      res.send("user saved");
+      //payload for jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      //to use jwt we need to sign
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       res.status(400).send("server error");
