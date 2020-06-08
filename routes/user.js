0f61fc5a8d8fe27083993acdec0bb3cd4496530b
Mohammed Ismail <miismail007@gmail.com>
2020-06-08 18:25:35 +0530

    register user with hash password

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 //register a user
-router.post("/", (req, res) => {
-  res.json({ msg: "welcome" });
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "please include valid email").isEmail(),
+    check("password", "please input with 6 or more character").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.json({ msg: "user already exist" });
+      }
+
+      user = User({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("user saved");
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).send("server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectdb = require("./config/db");
+
+connectdb();
+
+//init middleware to accept json data
+app.use(express.json({ extended: false }));
 
 app.post("/", (req, res) => {
   res.json({ msg: "welcome " });
